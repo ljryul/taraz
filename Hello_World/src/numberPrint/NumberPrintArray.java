690d7d08ea915ffb484b5db23a5542e750e92c10
ljryul <ljryul@gmail.com>
2019-10-11 17:58:21 +0900

    1부터 10까지 출력하기

---
+++ Hello_World/src/numberPrint/NumberPrintArray.java
@@ -0,0 +1,18 @@
+package numberPrint;
+
+/*
+ * Title : 1 ~ 10 number print by array
+ * Author : ljryul
+ */
+
+public class NumberPrintArray {
+
+	public static void main(String[] args) {
+		int arr[] = {1,2,3,4,5,6,7,8,9,10};
+		for(int i : arr) {
+			System.out.println(i);
+		}
+
+	}
+
+}
---
+++ Hello_World/src/numberPrint/NumberPrintFor.java
@@ -0,0 +1,16 @@
+package numberPrint;
+
+/*
+ * Title : 1 ~ 10 number print by for
+ * Author : ljryul
+ */
+
+public class NumberPrintFor {
+	public static void main(String[] args) {
+		for(int num = 1; num < 11; num++) {
+			System.out.println(num);
+		}
+				
+	}
+
+}
