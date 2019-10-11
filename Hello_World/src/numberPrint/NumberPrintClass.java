10b80e7c03bfa8dc406f4cf85e769ffbaee8c8be
ljryul <ljryul@gmail.com>
2019-10-11 17:59:33 +0900

    (작업중)배열에 숫자를 입력받아 출력해보기

---
+++ Hello_World/src/numberPrint/NumberPrintClass.java
@@ -0,0 +1,30 @@
+package numberPrint;
+
+public class NumberPrintClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		public static int[] arr = new int[10];
+		useScan();
+		useFor();
+		usdArr();
+
+	}
+
+	private static void useScan() {
+		// TODO Auto-generated method stub
+		
+		
+	}
+
+	private static void usdArr() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void useFor() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
