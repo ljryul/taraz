70dab0936bbdd1a282ec5acede86cdcf659d8628
ljryul <ljryul@gmail.com>
2019-10-16 17:17:39 +0900

    정수를 입력받아 2진수로 변환(바이트형태로 출력 예정)

---
+++ Hello_World/src/numToBinary/NumToBinary.java
@@ -0,0 +1,43 @@
+package numToBinary;
+
+import java.util.Scanner;
+
+/*
+ * Title : 10진수 숫자를 2진수로 바꾼뒤 byte 단위로 분할 출력
+ * Author : ljryul
+ */
+
+public class NumToBinary {
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		int num;
+
+		System.out.println("이진법으로 바꿀 숫자를 입력하세요");
+
+		// 숫자만 입력받고 숫자가 아닐경우 Exception
+		try {
+			num = scan.nextInt();
+			NumToBin(num);
+		} catch (Exception e) {
+			System.out.println("정수를 입력하세요");
+			e.printStackTrace();
+		}
+		scan.close();
+	}
+
+	private static void NumToBin(int num) {
+		/*
+		 * 입력된 숫자를 이진수로 변환하여 자릿수에 맞춰 0 삽입("%05d" 이 부분으로 자릿수 설정) String bin; bin =
+		 * String.format("%05d",
+		 * Integer.parseInt(Integer.toBinaryString(num).toString()));
+		 * System.out.printf("입력한 숫자 : %d%n이진변환 : %s",num,bin);
+		 */
+
+		//입력받은 숫자를 2진수로 변환(String타입)하여 String bin에 저장
+		String bin = Integer.toBinaryString(num);
+
+		System.out.println(bin);
+
+	}
+}
