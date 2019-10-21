80299dfa5468b61923cb42f4c8e2d3877ee0933d
ljryul <ljryul@gmail.com>
2019-10-21 17:51:47 +0900

    fix

--- Hello_World/src/numToBinary/NumToBinary.java
+++ Hello_World/src/numToBinary/NumToBinary.java
@@ -3,50 +3,54 @@ package numToBinary;
 import java.util.Scanner;
 
 /*
- * Title : 10진수 숫자를 2진수로 바꾼뒤 byte 단위로 분할 출력
+ * Title : 10진수 숫자를 2진수로 바꾼뒤 4자리씩 분할 출력(최대 4바이트)
  * Author : ljryul
+ * Special Thanks : BumNyeong Kim
  */
 
 public class NumToBinary {
 	public static void main(String[] args) {
 
 		Scanner scan = new Scanner(System.in);
-		Integer num = null;
+		int num;
 
-		System.out.println("이진법으로 바꿀 숫자를 입력하세요.(0을 입력하면 종료됩니다)");
+		System.out.println("이진법으로 바꿀 숫자를 입력하세요.(최대 2,147,483,647까지, 0을 입력하면 종료)");
 
+		// 숫자만 입력받고 숫자가 아닐경우 Exception 후 재입력
 		while (true) {
-			// 숫자만 입력받고 숫자가 아닐경우 Exception
-			
 			try {
 				num = scan.nextInt();
-				
-				if(num == 0) {
+
+				if (num == 0) {
+					System.out.println("프로그램 종료");
 					scan.close();
 					break;
 				}
-				
+
 				NumToBin(num);
 			} catch (Exception e) {
+				//e.printStackTrace();
 				System.out.println("정수를 입력하세요");
-				e.printStackTrace();
-				num = null;
+				scan.next();
 			}
-			
 		}
 	}
 
 	private static void NumToBin(int num) {
-		/*
-		 * 입력된 숫자를 이진수로 변환하여 자릿수에 맞춰 0 삽입("%05d" 이 부분으로 자릿수 설정) String bin; bin =
-		 * String.format("%05d",
-		 * Integer.parseInt(Integer.toBinaryString(num).toString()));
-		 * System.out.printf("입력한 숫자 : %d%n이진변환 : %s",num,bin);
-		 */
-
-		//입력받은 숫자를 2진수로 변환(String타입)하여 String bin에 저장
+		
+		// 입력받은 숫자를 2진수로 변환(String타입)하여 bin에 저장
 		String bin = Integer.toBinaryString(num);
 		System.out.println(bin);
 
+		// bin에 저장된 숫자를 4자리씩 끊어서 출력
+		String[] binNum;
+		binNum = bin.split("");
+		for (int i = 0; i < binNum.length; i++) {
+			System.out.printf(binNum[i]);
+			if ((i+1) % 4 == 0) {
+				System.out.printf(" ");
+			}
+		}
+		System.out.println("");
 	}
 }
