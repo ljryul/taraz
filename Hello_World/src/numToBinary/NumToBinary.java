22f4fbe89d308174ad4143e760586f4f75fce638
ljryul <ljryul@gmail.com>
2019-10-22 17:59:31 +0900

    정리 및 fix

--- Hello_World/src/numToBinary/NumToBinary.java
+++ Hello_World/src/numToBinary/NumToBinary.java
@@ -21,17 +21,18 @@ public class NumToBinary {
 			try {
 				num = scan.nextInt();
 
+				// 입력받은 숫자가 0이면 종료
 				if (num == 0) {
 					System.out.println("프로그램 종료");
 					scan.close();
 					break;
 				}
 
+				// 변환클래스 실행
 				NumToBin(num);
-				
-				System.out.println("\n2진변환 완료, 새로운 숫자를 입력하세요(0을 입력하면 종료합니다)");
+
+				System.out.println("\n변환 완료, 새로운 숫자를 입력하세요.(0을 입력하면 종료합니다)");
 			} catch (Exception e) {
-				// e.printStackTrace();
 				System.out.println("정수를 입력하세요");
 				scan.next();
 			}
@@ -40,79 +41,32 @@ public class NumToBinary {
 
 	private static void NumToBin(int num) {
 
-		String[] binNum;
-		//String[] intNum = new String[32];
-		
-		/*
-		// 0으로 초기화되어있는 8바이트 배열 선언
-		System.out.println("intNum배열 생성");
-		for (int i = 0; i < 32; i++) {
-			intNum[i] = "0";
-		}
-		*/
-
-		/*
-		// intNum 배열 확인
-		System.out.println("intNum배열 확인");
-		for(int i = 0; i <intNum.length ; i++) { 
-		 	System.out.printf(intNum[i] + " ");
-		 }
-		*/
-
 		// 입력받은 숫자를 2진수로 변환(String타입)하여 bin에 저장
-		System.out.println("\n변환시작");
+		System.out.println("변환시작");
 		String bin = Integer.toBinaryString(num);
-		int binCount = bin.length();
-		System.out.println("2진변환 : \n" + bin);
-		System.out.println("bin갯수 : \n" + binCount);
+		System.out.println("2진수 : " + bin);
 
-		// bin에 저장된 숫자를 binNum에 한자리씩 저장
-		System.out.println("binNum배열에 2진수를 저장함");
-		binNum = bin.split("");
-			
-		for(String b : binNum) {
-			System.out.printf(b);
-		}
-		
-		System.out.println();
-		
-		//StringBuffer에 32 - binCount 숫자만큼 0을 집어넣은 후 binNum 넣어서 출력
-		System.out.println("StringBuffer에 숫자 입력 시작");
+		// bin의 갯수 구함
+		int binCount = bin.length();
+
+		// StringBuffer에 32 - binCount 숫자만큼 0을 집어넣은 후 binNum 입력
 		StringBuffer sb = new StringBuffer();
 		for (int i = 0; i < 32 - binCount; i++) {
 			sb.append("0");
 		}
 		sb.append(bin);
-		System.out.println("입력된 숫자 : " +sb);
-		
+
+		// initNum 배열에 지금까지 입력된 숫자 하나씩 넣음
 		String[] initNum = sb.toString().split("");
+
+		// 결과물 출력
+		System.out.println("결과물 출력");
 		
-		System.out.println("새로 저장된 initNum 출력");
-		for(int i = 0; i <initNum.length ; i++) {
+		// 4자리마다 공백 추가
+		for (int i = 0; i < initNum.length; i++) {
 			System.out.printf(initNum[i]);
-			if((i+1)%4==0)	System.out.printf(" ");
+			if ((i + 1) % 4 == 0)
+				System.out.printf(" ");
 		}
-		
-		
-		
-		// binNum 내용 확인 및 4자리씩 끊어서 출력
-		/*
-		 for (int i = 0; i < binNum.length; i++) { System.out.printf(binNum[i]); if
-		 ((i+1) % 4 == 0) { System.out.printf(" "); } }
-		 */
-/*
-		// intNum 배열에 binNum 배열의 내용 덮어쓰기(8바이트형태로 출력하기 위함)
-		System.out.println("intNum배열에 binNum배열을 저장함");
-		for (int i = binNum.length-1; i >= 0; i--) {
-			intNum[i] = binNum[i];
-		}
-		
-		// intNum 출력
-		System.out.println("새로 저장된 intNum 출력");
-		for(int i = 0; i <intNum.length ; i++) {
-			System.out.printf(intNum[i]);
-			if((i+1)%4==0)	System.out.printf(" ");
-		}
-*/
 	}
 }
