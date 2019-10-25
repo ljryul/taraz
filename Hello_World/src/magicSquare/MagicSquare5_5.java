6fd508d1b3cc5a9df735295205004fe085da4287
ljryul <ljryul@gmail.com>
2019-10-25 13:38:15 +0900

    5*5 마방진

---
+++ Hello_World/src/magicSquare/MagicSquare5_5.java
@@ -0,0 +1,34 @@
+package magicSquare;
+
+public class MagicSquare5_5 {
+
+	public static void main(String[] args) {
+		int[][] ma = new int[5][5];
+		int row = 0, col = 2;
+
+		for (int i = 1; i <= 25; i++) {
+
+			ma[row][col] = i;
+
+			if (i % 5 == 0) {
+				row++;
+			} else {
+				row--;
+				col++;
+			}
+
+			if (row < 0)
+				row = 4;
+		
+			if (col > 4)
+				col = 0;
+		}
+		
+		for (int[] array : ma) {
+			for (int num : array) {
+				System.out.print(num + "\t");
+			}
+			System.out.println();
+		}
+	}
+}
