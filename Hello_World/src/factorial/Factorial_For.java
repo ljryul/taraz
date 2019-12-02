3a6dc976db65da5ec44f69c7f230df7802783eeb
ljryul <ljryul@gmail.com>
2019-12-02 16:38:22 +0900

    주석추가

--- Hello_World/src/factorial/Factorial_For.java
+++ Hello_World/src/factorial/Factorial_For.java
@@ -2,14 +2,21 @@ package factorial;
 
 import java.util.Scanner;
 
+/*
+ * Title : For문을 이용한 팩토리얼 계산
+ * Author : ljryul
+ */
+
 public class Factorial_For {
 
 	static public Scanner scan = new Scanner(System.in);
 
 	public static void main(String[] args) {
+		
 		int num;
 
 		System.out.println("숫자를 입력하세요.");
+		
 		try {
 			num = scan.nextInt();
 			System.out.println(num + " 팩토리얼은 " + factorial(num) + "입니다.");
@@ -21,6 +28,7 @@ public class Factorial_For {
 	}
 
 	public static int factorial(int num) {
+		
 		int sum = 1;
 
 		for (int i = 2; i <= num; i++) {
--- Hello_World/src/factorial/Factorial_Recursive.java
+++ Hello_World/src/factorial/Factorial_Recursive.java
@@ -2,14 +2,21 @@ package factorial;
 
 import java.util.Scanner;
 
+/*
+ * Title : 재귀함수를 이용한 팩토리얼 계산
+ * Author : ljryul
+ */
+
 public class Factorial_Recursive {
 
 	public static Scanner scan = new Scanner(System.in);
 
 	public static void main(String[] args) {
+
 		int num;
 
 		System.out.println("숫자를 입력하세요.");
+
 		try {
 			num = scan.nextInt();
 			System.out.println(num + " 팩토리얼은 " + factorial(num) + "입니다.");
@@ -19,6 +26,9 @@ public class Factorial_Recursive {
 		}
 	}
 
+	/*
+	 * 5!은 5 * 4 * 3 * 2 * 1 = 5 * 4!
+	 */
 	public static int factorial(int num) {
 
 		if (num == 1) {
